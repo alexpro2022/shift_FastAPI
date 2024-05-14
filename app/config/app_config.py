774c38302317ebcc6aeab36a5e1ede18bab75424
Alex Proskuriakov <alexpro1972@yandex.ru>
2024-05-14 18:38:15 +0300

    Create user and salary models with O2O relationship

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -10,15 +10,17 @@ class Settings(BaseConf):
 
     app_title: str = f"App title {DEFAULT_STR}"
     app_description: str = f"App description {DEFAULT_STR}"
-    secret_key: SecretStr = f"Secret key {DEFAULT_STR}"
+    salary_precision: int = 8
+    salary_scale: int = 2
 
     # authentication
+    secret_key: SecretStr = f"Secret key {DEFAULT_STR}"
     admin_email: EmailStr = "adm@adm.com"
-    admin_password: str = "adm"
+    admin_password: str = "admpw"
+    password_length: int = 3
     token_lifetime: int = 3600
     token_url: str = "auth/jwt/login"
     auth_backend_name: str = "jwt"
-    password_length: int = 3
 
 
 app_conf = Settings()
--- app/models/models.py
+++ app/models/models.py
@@ -1,16 +1,32 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
-
+from sqlalchemy import Numeric, UniqueConstraint
+from datetime import date
 from .base import Base
+import uuid
+from decimal import Decimal
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from config.app_config import app_conf
 
 
-class Example(Base):
-    title: Mapped[str] = mapped_column(String(256), unique=True, index=True)
-    description: Mapped[str] = mapped_column(String(2000), default="No description")
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    salary: Mapped["Salary"] = relationship(back_populates="user")
+
+
+class Salary(Base):
+    value: Mapped[Decimal] = mapped_column(Numeric(
+        precision=app_conf.salary_precision, scale=app_conf.salary_scale))
+    inc_date: Mapped[date]
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="salary")
+
+    __table_args__ = (UniqueConstraint("parent_id"),)
 
     def __repr__(self) -> str:
         return (
             f"{super().__repr__()}"
-            f"\ntitle: {self.title}"
-            f"\ndescription: {self.description}"
+            f"\nvalue: {self.value}"
+            f"\ninc_date: {self.inc_date}"
+            f"\nuser_id: {self.user_id}"
+            f"\nemail: {self.user.email}"
         )
